ba73334a45ebe82f7bc129e0f45522adc59304aa
NaweedNabi <102781741+NaweedNabi@users.noreply.github.com>
2022-10-08 23:21:22 -0700

    fixed some bugs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,7 +45,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/post/:id', (req, res) => {
+router.get('/post/:id',  (req, res) => {
     Post.findOne({
             where: {
                 id: req.params.id
